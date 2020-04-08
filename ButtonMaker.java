53da09a9989622aaf19d8671d6522d359b5f1043
starc52 <cstarpowful@gmail.com>
2020-04-09 05:18:24 +0530

    midway of implementing general button maker

---
+++ ButtonMaker.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+
+public class ButtonMaker {
+//    addParty = new JButton("Add Party");
+//    JPanel addPartyPanel = new JPanel();
+//		addPartyPanel.setLayout(new FlowLayout());
+//		addParty.addActionListener(this);
+//		addPartyPanel.add(addParty);
+//		controlsPanel.add(addPartyPanel);
+    public JPanel makeButtonWithPanel(String textToMatch){
+        JButton bttn = new JButton(textToMatch);
+        JPanel bttnPanel = new JPanel();
+        bttnPanel.setLayout(new FlowLayout());
+        bttn.addActionListener();
+
+    }
+}
